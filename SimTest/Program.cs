5ff2e25d7812d06e123fe3422ea7dcf6cec371c1
Andres Paz <anpaz@microsoft.com>
2018-01-30 11:30:31 -0800

    test project

--- SimTest/Program.cs
+++ SimTest/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SimTest
 {
     class Program
     {
+        public const string QSIM_DLL_NAME = "Microsoft.Quantum.Simulator.Runtime.dll";
+
+        [DllImport(QSIM_DLL_NAME, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, EntryPoint = "init")]
+        private static extern void Init();
+
+        [DllImport(QSIM_DLL_NAME, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, EntryPoint = "allocateQubit")]
+        private static extern void AllocateOne(uint id);
+
+        [DllImport(QSIM_DLL_NAME, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, EntryPoint = "release")]
+        private static extern bool ReleaseOne(uint id);
+
+        [DllImport(QSIM_DLL_NAME, ExactSpelling = true, CallingConvention = CallingConvention.Cdecl, EntryPoint = "X")]
+        private static extern void X(uint id);
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            uint id = 0;
+            Console.WriteLine("Hello!");
+            Init();
+
+            Console.WriteLine("Do something...");
+            AllocateOne(id);
+            X(id);
+            ReleaseOne(id);
+
+            Console.WriteLine("Test complete.");
         }
     }
 }
